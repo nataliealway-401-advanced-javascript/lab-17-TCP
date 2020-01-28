c9962ddbb1058cb41bdfdcf1d96531f8d3ac974d
natalielyn <nlmaxwell@gmail.com>
2020-01-28 15:21:18 -0800

    one test passing

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@
 // SERVER HUB APPLICATION
 //----------------------------------------------------
 const net = require('net');
-const events = require('../../events.js');
+const events = require('../events.js');
 const port = 3001;
 const server = net.createServer();
 
@@ -12,14 +12,15 @@ server.listen(port, () => console.log('server up on port:', port));
 let socketPool = {};
 
 
-
 server.on('connection', (socket) => {
   const id = `Socket-${Math.random()}`;
   socketPool[id] = socket;
+  console.log('Welcome', id);
   socket.on('data', (buffer) => events.emit('handleEvents', buffer));
   socket.on('error', (e) => {console.log('socket error', e);});
   socket.on('close', () => {
     delete socketPool[id];
+    console.log(`Goodbye ${id}`);
   });
 });
 
